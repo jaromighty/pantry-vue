7af4139bbd75922e710f10d82b744433ba55c445
Jarom Rial <jarom.rial@gmail.com>
2022-07-28 19:11:27 -0600

    add recipes index page

---
+++ resources/js/Pages/Recipes/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+import Header from '@/Components/Headers/Index.vue';
+import PrimaryButton from '@/Components/Buttons/Primary.vue';
+</script>
+
+<template>
+    <Head title="Recipes" />
+
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <Header>
+                <template #title>
+                    Recipes
+                </template>
+                <template #actions>
+                    <PrimaryButton type="button">
+                        Add a recipe
+                    </PrimaryButton>
+                </template>
+            </Header>
+        </div>
+    </AuthenticatedLayout>
+</template>
