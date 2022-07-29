fb3f8b2bdfc54443cdc010747e2633a73bc5eb7f
Jarom Rial <jarom.rial@gmail.com>
2022-07-28 19:10:28 -0600

    add header and primary button components

---
+++ resources/js/Components/Headers/Index.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+    <header class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="md:flex md:items-center md:justify-between">
+            <div class="flex-1 min-w-0">
+                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                    <slot name="title"/>
+                </h2>
+            </div>
+            <div class="mt-4 flex md:mt-0 md:ml-4">
+                <slot name="actions" />
+            </div>
+        </div>
+    </header>
+</template>
--- resources/js/Pages/Ingredients/Index.vue
+++ resources/js/Pages/Ingredients/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import Header from '@/Components/Headers/Index.vue';
+import PrimaryButton from '@/Components/Buttons/Primary.vue';
 </script>
 
 <template>
@@ -8,16 +10,16 @@ import { Head } from '@inertiajs/inertia-vue3';
 
     <AuthenticatedLayout>
         <div class="py-12">
-            <header class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="md:flex md:items-center md:justify-between">
-                    <div class="flex-1 min-w-0">
-                        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Ingredients</h2>
-                    </div>
-                    <div class="mt-4 flex md:mt-0 md:ml-4">
-                        <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add an ingredient</button>
-                    </div>
-                </div>
-            </header>
+            <Header>
+                <template #title>
+                    Ingredients
+                </template>
+                <template #actions>
+                    <PrimaryButton type="button">
+                        Add an ingredient
+                    </PrimaryButton>
+                </template>
+            </Header>
         </div>
     </AuthenticatedLayout>
 </template>
