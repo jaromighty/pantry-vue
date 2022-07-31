6c83dddacdd58cc3bd2f60b616a72013879c98a3
Jarom Rial <jarom.rial@gmail.com>
2022-07-31 09:48:07 -0600

    display ingredient list on condensed table

--- resources/js/Pages/Ingredients/Index.vue
+++ resources/js/Pages/Ingredients/Index.vue
@@ -24,6 +24,39 @@ defineProps({
                     </PrimaryButton>
                 </template>
             </Header>
+            <div class="max-w-7xl mx-auto">
+                <div class="px-4 sm:px-6 lg:px-8">
+                    <div class="mt-6 flex flex-col">
+                        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-300">
+                                        <thead class="bg-gray-50">
+                                            <tr>
+                                                <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                                                <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6">
+                                                    <span class="sr-only">Edit</span>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="divide-y divide-gray-200 bg-white">
+                                            <tr v-for="ingredient in ingredients" :key="ingredient.id">
+                                                <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">{{ ingredient.name }}</td>
+                                                <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">
+                                                        Edit
+                                                        <span class="sr-only">, {{ ingredient.id }}</span>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
