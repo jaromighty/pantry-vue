55525f8207c790814121f2e19b0dc995cb437d07
Jarom Rial <jarom.rial@gmail.com>
2022-07-31 08:03:47 -0600

    add header for create pages

---
+++ resources/js/Components/Headers/Create.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">
+            <slot />
+        </h1>
+    </header>
+</template>
