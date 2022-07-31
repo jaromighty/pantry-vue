f758d6f808f9c04e38187dd75150eecacded9f2f
Jarom Rial <jarom.rial@gmail.com>
2022-07-31 09:14:19 -0600

    build front form for adding ingredients

--- resources/js/Pages/Ingredients/Create.vue
+++ resources/js/Pages/Ingredients/Create.vue
@@ -1,7 +1,16 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head, useForm } from '@inertiajs/inertia-vue3';
 import Header from '@/Components/Headers/Create.vue';
+import PrimaryButton from '@/Components/Buttons/Primary.vue';
+
+const form = useForm({
+    name: ""
+});
+
+const submit = () => {
+    form.post(route('ingredients.store'));
+}
 </script>
 
 <template>
@@ -10,6 +19,19 @@ import Header from '@/Components/Headers/Create.vue';
     <AuthenticatedLayout>
         <div class="py-12">
             <Header>Add an ingredient</Header>
+            <div class="mt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <form @submit="submit">
+                    <div>
+                        <label for="ingredient-name" class="block text-sm font-medium text-gray-700">Ingredient name</label>
+                        <div class="mt-1">
+                            <input type="text" v-model="name" id="ingredient-name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <PrimaryButton>Save</PrimaryButton>
+                    </div>
+                </form>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
