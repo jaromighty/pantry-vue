272c83445e304ba05d7a6705ac7d74faa340be24
Jarom Rial <jarom.rial@gmail.com>
2022-11-29 20:55:40 -0700

    view a single recipe

--- resources/js/Components/Lists/RecipeItem.vue
+++ resources/js/Components/Lists/RecipeItem.vue
@@ -1,15 +1,19 @@
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
 defineProps(['recipe']);
 </script>
 
 <template>
     <li class="relative">
-        <div
-            class="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-200 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-            <!-- <img :src="file.source" alt="" class="pointer-events-none object-cover group-hover:opacity-75" /> -->
-            <button type="button" class="absolute inset-0 focus:outline-none" />
-        </div>
-        <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{{ recipe.name }}</p>
-        <!-- <p class="pointer-events-none block text-sm font-medium text-gray-500">{{ file.size }}</p> -->
+        <Link :href="route('recipes.show', [recipe.id])">
+            <div
+                class="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-200 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                <!-- <img :src="file.source" alt="" class="pointer-events-none object-cover group-hover:opacity-75" /> -->
+                <button type="button" class="absolute inset-0 focus:outline-none" />
+            </div>
+            <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{{ recipe.name }}</p>
+            <!-- <p class="pointer-events-none block text-sm font-medium text-gray-500">{{ file.size }}</p> -->
+        </Link>
     </li>
 </template>
---
+++ resources/js/Pages/Recipes/Show.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import Header from '@/Components/Headers/Index.vue';
+import PrimaryButton from '@/Components/Buttons/Primary.vue';
+
+defineProps(['recipe'])
+</script>
+
+<template>
+
+    <Head :title="recipe.name" />
+
+    <AuthenticatedLayout>
+        <div>
+            <div>
+                <img class="h-36 w-full object-cover lg:h-52"
+                    src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80"
+                    alt="" />
+            </div>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="sm:flex sm:items-end sm:space-x-5">
+                    <div class="sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                        <div class="mt-3 min-w-0 flex-1 sm:hidden md:block">
+                            <h1 class="truncate text-2xl font-bold text-gray-900">{{ recipe.name }}</h1>
+                        </div>
+                        <div class="justify-stretch mt-3 flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                            <PrimaryButton as="link" :href="route('recipes.edit', [recipe.id])">
+                                Edit
+                            </PrimaryButton>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-3 hidden min-w-0 flex-1 sm:block md:hidden">
+                    <h1 class="truncate text-2xl font-bold text-gray-900">{{ recipe.name }}</h1>
+                </div>
+            </div>
+
+            <div class="mt-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="overflow-hidden rounded-lg bg-gray-200">
+                    <div class="px-4 py-5 sm:p-6">
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">Ingredients</h3>
+                    </div>
+                </div>
+
+                <div class="mt-6">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">Instructions</h3>
+                    <div class="prose" v-html="recipe.instructions" />
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
